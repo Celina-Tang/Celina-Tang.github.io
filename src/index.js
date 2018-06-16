10e1f2c6a5c15d7c31f5ab007fecbf6c0e1b679c
Celina-Tang <tangcelina12@hotmail.ca>
2018-06-16 12:35:09 -0400

    Re-route to celina-tang.github.io and able to pulish website correctly

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MainPage from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
+import 'bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap';
 
-ReactDOM.render(<MainPage />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><MainPage /></Router>, document.getElementById('root'));
 registerServiceWorker();
